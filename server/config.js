f432a091beb169ff38b81a08d6e07e24bd47da41
david Henriquez <152029007+jedavid99@users.noreply.github.com>
2024-07-15 17:39:00 -0300

    update

--- server/config.js
+++ server/config.js
@@ -24,9 +24,3 @@ export const POSTGRES_URL = process.env.POSTGRES_URL
 
 
 
-// export const PORT = process.env.PORT || 3001
-// export const DB_USER = process.env.DB_USER || 'postgres'
-// export const DB_PASSWORD = process.env.DB_PASSWORD || '1234'
-// export const DB_HOST = process.env.DB_HOST || 'localhost'
-// export const DB_DATABASE = process.env.DB_DATABASE || 'geeksmart'
-// export const DB_PORT = process.env.DB_PORT || 5432
